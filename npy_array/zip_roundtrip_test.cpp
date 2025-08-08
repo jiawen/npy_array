bb4aefed0aa62481bb3f3132966c670765dbf49e
Jiawen Chen <jiawen@mit.edu>
2025-08-08 15:36:10 -0700

    add method and level test

--- npy_array/zip_roundtrip_test.cpp
+++ npy_array/zip_roundtrip_test.cpp
@@ -10,35 +10,66 @@
 using ::absl_testing::IsOk;
 using ::absl_testing::IsOkAndHolds;
 using ::testing::Eq;
+using ::testing::Gt;
 using ::testing::IsEmpty;
+using ::testing::Lt;
 using ::testing::SizeIs;
 
 namespace npy_array {
 namespace {
 
-#if 0
-TEST(ZipRoundtripTest, Foo) {
+TEST(ZipWriterTest, CanUseStore) {
   ZipWriter zip_writer;
-  EXPECT_THAT(zip_writer.AddFile("hello.txt", "hello",
-  EXPECT_THAT(zip_writer.AddFile("dir/world.txt", data,
+
+  constexpr size_t kDataSize = 1024 * 1024;
+  const std::string kData(kDataSize, 'a');  // 1 MB of 'a's.
+
+  EXPECT_THAT(zip_writer.AddFile("hello.txt", kData,
                                  ZipWriter::AddFileOptions{
-                                     .method = ZipMethod::kDeflate,
-                                     .level = 9,
+                                     .method = ZipMethod::kStore,
                                  }),
               IsOk());
 
+  // The stored data is slightly larger than the original data.
   absl::StatusOr<std::string> maybe_data = std::move(zip_writer).Close();
-
-  EXPECT_THAT(maybe_data, IsOk());
-
-  absl::StatusOr<absl::flat_hash_map<std::filesystem::path, std::string>>
-      maybe_contents = ReadZipFile(*maybe_data);
-  EXPECT_THAT(maybe_contents, IsOk());
-  EXPECT_THAT(maybe_contents->size(), Eq(2));
-  EXPECT_THAT(maybe_contents->at("hello.txt"), Eq("hello"));
-  EXPECT_THAT(maybe_contents->at("dir/world.txt"), Eq(data));
+  EXPECT_THAT(maybe_data, IsOkAndHolds(SizeIs(Gt(kData.size()))));
 }
-#endif
+
+TEST(ZipWriterTest, CanUseDeflate) {
+  ZipWriter zip_writer_level_1;
+  ZipWriter zip_writer_level_9;
+
+  constexpr size_t kDataSize = 1024 * 1024;
+  const std::string kData(kDataSize, 'a');  // 1 MB of 'a's.
+
+  EXPECT_THAT(zip_writer_level_1.AddFile("hello.txt", kData,
+                                         ZipWriter::AddFileOptions{
+                                             .method = ZipMethod::kDeflate,
+                                             .level = 1,
+                                         }),
+              IsOk());
+
+  EXPECT_THAT(zip_writer_level_9.AddFile("hello.txt", kData,
+                                         ZipWriter::AddFileOptions{
+                                             .method = ZipMethod::kDeflate,
+                                             .level = 9,
+                                         }),
+              IsOk());
+
+  // The compressed data at level 1 is smaller than the original data.
+  absl::StatusOr<std::string> maybe_data_level_1 =
+      std::move(zip_writer_level_1).Close();
+  EXPECT_THAT(maybe_data_level_1, IsOkAndHolds(SizeIs(Lt(kData.size()))));
+
+  // The compressed data at level 9 is smaller than the original data.
+  absl::StatusOr<std::string> maybe_data_level_9 =
+      std::move(zip_writer_level_9).Close();
+  EXPECT_THAT(maybe_data_level_9, IsOkAndHolds(SizeIs(Lt(kData.size()))));
+
+  // The compressed data at level 9 is smaller than the compressed data at level
+  // 1.
+  EXPECT_LT(maybe_data_level_9->size(), maybe_data_level_1->size());
+}
 
 TEST(ZipRoundtripTest, CanWriteZipFileWithNoContents) {
   ZipWriter zip_writer;
